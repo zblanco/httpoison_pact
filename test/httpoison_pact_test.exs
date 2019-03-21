605ae71f369f5d03e6d09b961f5fc0e55ad9f693
Zack <zkwhited@gmail.com>
2019-03-21 11:51:04 -0600

    add sanity check test

--- test/httpoison_pact_test.exs
+++ test/httpoison_pact_test.exs
@@ -2,7 +2,17 @@ defmodule HttpoisonPactTest do
   use ExUnit.Case
   doctest HttpoisonPact
 
-  test "greets the world" do
-    assert HttpoisonPact.hello() == :world
+  test "can make a simple request against google.com" do
+    request = %HTTPact.Request{
+      method: :get,
+      headers: [{"Content-Type", "text/html; charset=utf-8"}],
+      path: "https://www.google.com",
+      http_client: HttpoisonPact,
+    }
+
+    {:ok, response} = HTTPact.execute(request)
+
+    assert response.status == 200
+    assert response.body != nil
   end
 end
